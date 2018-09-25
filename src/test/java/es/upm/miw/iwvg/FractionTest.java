e039e495c93f51220fd3c64c1b54d4c60ac202b4
ATorrecilla <a.torrecilla@alumnos.upm.es>
2018-09-25 20:16:31 +0200

    commit issue#5 corregido

--- src/test/java/es/upm/miw/iwvg/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg/FractionTest.java
@@ -2,6 +2,7 @@ package es.upm.miw.iwvg;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
